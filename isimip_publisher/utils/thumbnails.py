bd62b0d68e565e7d09047d48a25d7b8b48836b74
Jochen Klar <mail@jochenklar.de>
2019-10-31 20:05:17 +0100

    Fix thumbnail creation

--- isimip_publisher/utils/thumbnails.py
+++ isimip_publisher/utils/thumbnails.py
@@ -23,21 +23,21 @@ def write_file_thumbnail(file_abspath, output_abspath=None):
             if len(variable.dimensions) > 1:
                 break
 
-        plt.axes(projection=ccrs.PlateCarree()).coastlines()
-        plt.title(var_name)
-
         try:
             lat = dataset.variables['lat'][:]
             lon = dataset.variables['lon'][:]
             var = dataset.variables[var_name][-1, :, :]
 
+            plt.clf()
+            plt.axes(projection=ccrs.PlateCarree()).coastlines()
             plt.contourf(lon, lat, var, LEVELS, transform=ccrs.PlateCarree())
+            plt.title(var_name)
         except (IndexError, ValueError):
-            pass
+            # reset plot
+            plt.clf()
+            plt.axes(projection=ccrs.PlateCarree()).coastlines()
+            plt.title(var_name)
 
         fig = plt.gcf()
         fig.set_size_inches(WIDTH/DPI, HEIGHT/DPI)
         fig.savefig(output_abspath, dpi=DPI)
-
-        # reset plt
-        plt.clf()
