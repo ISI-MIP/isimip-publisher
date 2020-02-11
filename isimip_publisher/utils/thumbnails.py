2f0e1f9122d9fee9fd2a1abed8957ea8aadc88fd
Jochen Klar <mail@jochenklar.de>
2020-02-11 19:05:30 +0100

    Fix thumbnail creation

--- isimip_publisher/utils/thumbnails.py
+++ isimip_publisher/utils/thumbnails.py
@@ -1,10 +1,12 @@
+import logging
 import os
 import shutil
 
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
-import numpy.ma as ma
 from netCDF4 import Dataset
+
+logger = logging.getLogger(__name__)
 
 WIDTH = 800
 HEIGHT = 380
@@ -17,6 +19,8 @@ def write_dataset_thumbnail(dataset_abspath, files):
 
 
 def write_file_thumbnail(file_abspath, output_abspath=None):
+    mock = os.environ.get('MOCK', '').lower() in ['t', 'true', 1]
+
     if not output_abspath:
         output_abspath = file_abspath.replace('.nc4', '.png')
 
@@ -26,11 +30,11 @@ def write_file_thumbnail(file_abspath, output_abspath=None):
             if len(variable.dimensions) > 1:
                 break
 
-        try:
-            if dataset.variables[var_name][-1, 0, 0] is not ma.masked:
+        if not mock:
+            try:
                 lat = dataset.variables['lat'][:]
                 lon = dataset.variables['lon'][:]
-                var = dataset.variables[var_name][-1, :, :]
+                var = dataset.variables[var_name][1, :, :]
 
                 plt.clf()
                 plt.axes(projection=ccrs.PlateCarree()).coastlines()
@@ -42,8 +46,8 @@ def write_file_thumbnail(file_abspath, output_abspath=None):
                 fig.savefig(output_abspath, dpi=DPI)
 
                 return
-        except (IndexError, ValueError):
-            pass
+            except (IndexError, ValueError) as e:
+                logger.warn(e)
 
         # if plotting did not work, copy empty.png
         empty_file = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'extras', 'empty.png')
