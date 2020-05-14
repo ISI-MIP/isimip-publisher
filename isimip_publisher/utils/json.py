2e40b462f141876b99146122eb9a92cba73c33dc
Jochen Klar <mail@jochenklar.de>
2020-05-14 17:48:05 +0200

    Add path, checksum, checksum_type to json

--- isimip_publisher/utils/json.py
+++ isimip_publisher/utils/json.py
@@ -1,6 +1,8 @@
 import json as python_json
 import logging
 
+from .checksum import get_checksum_type, get_file_checksum
+
 logger = logging.getLogger(__name__)
 
 
@@ -8,6 +10,12 @@ def write_file_json(config, file, attributes):
     json_path = file['abspath'].with_suffix('.json')
 
     logger.info('write_file_json %s', json_path)
+
+    attributes.update({
+        'path': str(file['path']),
+        'checksum':  get_file_checksum(file),
+        'checksum_type':  get_checksum_type()
+    })
 
     with open(json_path, 'w') as f:
         f.write(python_json.dumps(attributes, indent=2))
