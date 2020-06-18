4ffefb50b6c70482273ceb4a7496eda89033a075
Jochen Klar <mail@jochenklar.de>
2020-06-18 10:33:17 +0200

    Fix write_thumbnail for non NetCDF files

--- isimip_publisher/utils/thumbnails.py
+++ isimip_publisher/utils/thumbnails.py
@@ -23,31 +23,34 @@ def write_thumbnail(file, output_abspath=None):
 
     logger.info('write_file_thumbnail %s', output_abspath)
 
-    with Dataset(file['abspath'], mode='r') as dataset:
+    try:
+        with Dataset(file['abspath'], mode='r') as dataset:
 
-        for var_name, variable in dataset.variables.items():
-            if len(variable.dimensions) > 1:
-                break
+            for var_name, variable in dataset.variables.items():
+                if len(variable.dimensions) > 1:
+                    break
 
-        if not mock:
-            try:
-                lat = dataset.variables['lat'][:]
-                lon = dataset.variables['lon'][:]
-                var = dataset.variables[var_name][1, :, :]
+            if not mock:
+                try:
+                    lat = dataset.variables['lat'][:]
+                    lon = dataset.variables['lon'][:]
+                    var = dataset.variables[var_name][1, :, :]
 
-                plt.clf()
-                plt.axes(projection=ccrs.PlateCarree()).coastlines()
-                plt.contourf(lon, lat, var, LEVELS, transform=ccrs.PlateCarree())
-                plt.title(var_name)
+                    plt.clf()
+                    plt.axes(projection=ccrs.PlateCarree()).coastlines()
+                    plt.contourf(lon, lat, var, LEVELS, transform=ccrs.PlateCarree())
+                    plt.title(var_name)
 
-                fig = plt.gcf()
-                fig.set_size_inches(WIDTH/DPI, HEIGHT/DPI)
-                fig.savefig(output_abspath, dpi=DPI)
+                    fig = plt.gcf()
+                    fig.set_size_inches(WIDTH/DPI, HEIGHT/DPI)
+                    fig.savefig(output_abspath, dpi=DPI)
 
-                return
-            except (IndexError, ValueError) as e:
-                logger.warn(e)
+                    return
+                except (IndexError, ValueError) as e:
+                    logger.warn(e)
+    except OSError:
+        pass
 
-        # if plotting did not work, copy empty.png
-        empty_file = Path(__file__).parent.parent / 'extras' / 'empty.png'
-        shutil.copyfile(empty_file, output_abspath)
+    # if plotting did not work, copy empty.png
+    empty_file = Path(__file__).parent.parent / 'extras' / 'empty.png'
+    shutil.copyfile(empty_file, output_abspath)
