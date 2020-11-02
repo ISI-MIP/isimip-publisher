7e1a9ffd9e694575f6296e7e64de4f1440d45a95
Jochen Klar <mail@jochenklar.de>
2020-11-02 17:55:41 +0100

    Add tree.py

--- isimip_publisher/utils/database.py
+++ isimip_publisher/utils/database.py
@@ -9,6 +9,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 
 from .datacite import gather_datasets
+from .tree import build_tree_dict, build_tree_list
 
 logger = logging.getLogger(__name__)
 
@@ -340,32 +341,6 @@ def update_tree(session):
             tree_list=tree_list
         )
         session.add(tree)
-
-
-def build_tree_dict(tree_dict, specifiers):
-    identifier, specifier = specifiers[0]
-
-    if specifier not in tree_dict:
-        tree_dict[specifier] = {
-            'identifier': identifier,
-            'specifier': specifier,
-            'items': {}
-        }
-
-    if len(specifiers) > 1:
-        build_tree_dict(tree_dict[specifier]['items'], specifiers[1:])
-
-
-def build_tree_list(tree_dict):
-    tree_list = []
-    for specifier, item in tree_dict.items():
-        tree_list.append({
-            'identifier': item.get('identifier'),
-            'specifier': item.get('specifier'),
-            'items': build_tree_list(item.get('items'))
-        })
-
-    return tree_list
 
 
 def update_words_view(session):
