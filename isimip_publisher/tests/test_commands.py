4bf4317affa4dc3e902be11cf5f06f339517008e
Jochen Klar <mail@jochenklar.de>
2021-01-16 13:43:01 +0100

    Fix tests

--- isimip_publisher/tests/test_commands.py
+++ isimip_publisher/tests/test_commands.py
@@ -2,8 +2,9 @@ import os
 import shutil
 from pathlib import Path
 
-import pytest
 from dotenv import load_dotenv
+
+import pytest
 from isimip_publisher.utils.database import (Dataset, Resource,
                                              init_database_session)
 
@@ -131,18 +132,18 @@ def test_match_public(setup, script_runner):
     assert not response.stderr
 
 
-def test_insert_resources(setup, script_runner):
-    response = script_runner.run('isimip-publisher', 'insert_resources', 'testing/resources/ISIMIP.001.json')
+def test_insert_doi(setup, script_runner):
+    response = script_runner.run('isimip-publisher', 'insert_doi', 'testing/resources/ISIMIP.001.json')
     assert response.success, response.stderr
     assert not response.stdout
     assert not response.stderr
 
 
-def test_update_resources(setup, script_runner):
-    response = script_runner.run('isimip-publisher', 'update_resources', 'testing/resources/ISIMIP.001.json')
+def test_update_doi(setup, script_runner):
+    response = script_runner.run('isimip-publisher', 'update_doi', 'testing/resources/ISIMIP.001.json')
     assert response.success, response.stderr
     assert not response.stdout
-    assert response.stderr  # version warning
+    assert not response.stderr
 
 
 def test_update_index(setup, script_runner):
